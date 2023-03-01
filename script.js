d158d670a090ca69cd8c99fbd16cd2aac55cd523
Svec Antal <anton.svec13@gmail.com>
2023-03-01 13:09:00 +0100

    save

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-h
+var playerSign = null
+var computerSignContainer = document.querySelector(
+    '.field.computer selected-sign'
+)
+var playerSignContainer = document.querySelector('.field.player selected-sign')
+
+function playerSelectSign(sign) {
+    if (playerSign != null) {
+        playerSign = sign
+        playGame()
+    }
+}
+
+function playGame() {}
